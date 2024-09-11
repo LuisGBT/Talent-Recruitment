ac32990e68aea01548b9c8959a7c12e7083fce6d
LuisGBT <luizingustavo507@gmail.com>
2024-09-11 13:14:09 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal({reset: true});
+sr.reveal('h1', {
+  rotate: {x: 0, y:80, z:0},
+  duration: 2000
+});
+
+sr.reveal('h2', {
+  rotate: {x: 0, y:80, z:0},
+  duration: 1000
+});
+
+sr.reveal('h3', {
+  rotate: {x: 0, y:80, z:0},
+  duration: 1000
+});
+
+sr.reveal('button', {
+  rotate: {x: 0, y:80, z:0},
+  duration: 1000
+});
+
+sr.reveal('lord-icon', {
+  rotate: {x: 0, y:80, z:0},
+  duration: 1000
+});
+
+sr.reveal('p', {
+  rotate: {x: 0, y:80, z:0},
+  duration: 1000
+});
+
+sr.reveal('img', {
+  rotate: {x: 100, y:100, z:0},
+  duration: 1000
+});
